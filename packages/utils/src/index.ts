b52cf01eb5d0355a6cc05f3e0dcf8ff89cbec6c7
dadajam4 <dadajam4@gmail.com>
2023-03-02 18:52:40 +0900

    feat(utils): add sayHelloByName fn

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,3 +2,13 @@ import './styles.css';
 
 export * from './schemes';
 export * from './utils';
+
+/**
+ * Say Hello.
+ *
+ * @param name - name
+ * @returns Hello string.
+ */
+export function sayHelloByName<T extends string>(name: T) {
+  return `Hello ${name}.` as const;
+}
